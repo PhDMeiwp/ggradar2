121be40704701b35744f6e8f34d42e70eddf5287
PhDMeiwp <meiweipingg@163.com>
2024-12-04 10:17:19 +0800

    ggradar2 version 0.3

--- push to github.bat
+++ push to github.bat
@@ -2,5 +2,5 @@ git init
 git remote remove origin
 git remote add origin git@github.com:PhDMeiwp/ggradar2.git
 git add .
-git commit -m "Depends: R version >= 4.3.2"
+git commit -m "ggradar2 version 0.3"
 git push origin master -f
